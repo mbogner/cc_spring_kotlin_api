204e4926118244212120a13fb1b734f067fb6495
Manuel Bogner <manuel.bogner@openresearch.com>
2024-10-26 00:31:58 +0200

    we need test folder but not the one in root dir

---
+++ {{cookiecutter.project_name}}/api/src/test/kotlin/{{cookiecutter.package_dir}}/ApiTest.kt
@@ -0,0 +1,14 @@
+package {{cookiecutter.package}}
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiTest {
+
+    @Test
+    internal fun contextLoads() {
+        // do nothing
+    }
+
+}
